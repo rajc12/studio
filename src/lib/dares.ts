c6b373de0f4e970e0e0d23c56cef0c04ae7d009d
devansh pandya <devanshpandya15@gmail.com>
2025-09-26 19:33:39 +0000

    dares

--- src/lib/dares.ts
+++ src/lib/dares.ts
@@ -1,12 +1,3 @@
 export const DARES = [
-  "Sing the chorus of your favorite song out loud.",
-  "Speak in a silly accent until your next turn.",
-  "Do 10 jumping jacks right now.",
-  "Let the player who played the draw card see your hand.",
-  "Play with your cards revealed to everyone until your next turn.",
-  "Tell a funny joke. If no one laughs, draw a card.",
-  "Compliment the player to your left.",
-  "Act like a T-Rex until your next turn.",
-  "Use 'yee-haw' in every sentence until you play your next card.",
-  "Don't speak until your next turn. Use only gestures.",
+  "strip a article of clothing ", "get spank by both player on one each cheek ", "get groped by both player ", "flash both the player","dry-hump the player of your choice",
 ];
